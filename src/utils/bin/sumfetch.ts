ed762256e6c5bd48cc43e4d63311dd1e544b8add
SteamWolf <85211822+SteamWo1f@users.noreply.github.com>
2023-01-18 19:30:05 -0700

    Update 2.4.1 (Reformatted "◊" to "♦")

--- src/utils/bin/sumfetch.ts
+++ src/utils/bin/sumfetch.ts
@@ -18,17 +18,17 @@ const summary = async (args: string[]): Promise<string> => {
   | 爵 <u><a href="https://drive.google.com/drive/folders/13i6pVkjvJCkA6qjiBb4cJNPptSpuPLOZ?usp=sharing" target="_blank">Portfolio</a></u>  
  |  ----------- 
   |  CONTACT 
- |  ◊ <u><a href="mailto:${config.email}" target="_blank">${config.email}</a></u> 
-  | ◊ <u><a href="https://github.com/${config.social.github}" target="_blank">Github</a></u>
- |  ◊ <u><a href="https://linkedin.com/in/${config.social.linkedin}" target="_blank">Linkedin</a></u>
-  | ◊ <u><a href="https://www.reddit.com/user/SteamWo1f" target="_blank">Reddit</a></u>
- |  ◊ <u><a href="https://twitter.com/SteamWolf5" target="_blank">Twitter</a></u>
-  | ◊ <u><a href="https://www.twitch.tv/steamwo1f" target="_blank">Twitch</a></u>
- |  ◊ <u><a href="https://steamwolf.bandcamp.com/" target="_blank">Music</a></u>
-  | ◊ <u><a href="https://www.tumblr.com/blog/steamwo1f" target="_blank">Tumblr</a></u>
- |  ◊ <u><a href="https://discord.com/invite/XBz9hJAxzD" target="_blank">Discord Server</a></u>
-  | ◊ <u><a href="https://www.youtube.com/channel/UCJqYptKIPNtsWRYWJZZH5Dg" target="_blank">YouTube</a></u>
- |  ◊ <u><a href="https://archiveofourown.org/users/SteamWolf" target="_blank">Ao3</a></u>  
+ |  ♦ <u><a href="mailto:${config.email}" target="_blank">${config.email}</a></u> 
+  | ♦ <u><a href="https://github.com/${config.social.github}" target="_blank">Github</a></u>
+ |  ♦ <u><a href="https://linkedin.com/in/${config.social.linkedin}" target="_blank">Linkedin</a></u>
+  | ♦ <u><a href="https://www.reddit.com/user/SteamWo1f" target="_blank">Reddit</a></u>
+ |  ♦ <u><a href="https://twitter.com/SteamWolf5" target="_blank">Twitter</a></u>
+  | ♦ <u><a href="https://www.twitch.tv/steamwo1f" target="_blank">Twitch</a></u>
+ |  ♦ <u><a href="https://steamwolf.bandcamp.com/" target="_blank">Music</a></u>
+  | ♦ <u><a href="https://www.tumblr.com/blog/steamwo1f" target="_blank">Tumblr</a></u>
+ |  ♦ <u><a href="https://discord.com/invite/XBz9hJAxzD" target="_blank">Discord Server</a></u>
+  | ♦ <u><a href="https://www.youtube.com/channel/UCJqYptKIPNtsWRYWJZZH5Dg" target="_blank">YouTube</a></u>
+ |  ♦ <u><a href="https://archiveofourown.org/users/SteamWolf" target="_blank">Ao3</a></u>  
   | -----------
  |   DONATE 
   |  <u><a href="https://paypal.me/SteamWo1f" target="_blank">PayPal</a></u>
@@ -52,17 +52,17 @@ const summary = async (args: string[]): Promise<string> => {
     | 爵 <u><a href="https://drive.google.com/drive/folders/13i6pVkjvJCkA6qjiBb4cJNPptSpuPLOZ?usp=sharing" target="_blank">Portfolio</a></u>  
    |  ----------- 
     |  CONTACT 
-   |   <u><a href="mailto:${config.email}" target="_blank">${config.email}</a></u> 
-    |  <u><a href="https://github.com/${config.social.github}" target="_blank">Github</a></u>
-   |   <u><a href="https://linkedin.com/in/${config.social.linkedin}" target="_blank">Linkedin</a></u>
-    | 爵 <u><a href="https://www.reddit.com/user/SteamWo1f" target="_blank">Reddit</a></u>
-   |  爵 <u><a href="https://twitter.com/SteamWolf5" target="_blank">Twitter</a></u>
-    | 爵 <u><a href="https://www.twitch.tv/steamwo1f" target="_blank">Twitch</a></u>
-   |  爵 <u><a href="https://steamwolf.bandcamp.com/" target="_blank">Music</a></u>
-    | 爵 <u><a href="https://www.tumblr.com/blog/steamwo1f" target="_blank">Tumblr</a></u>
-   |  爵 <u><a href="https://discord.com/invite/XBz9hJAxzD" target="_blank">Discord Server</a></u>
-    | 爵 <u><a href="https://www.youtube.com/channel/UCJqYptKIPNtsWRYWJZZH5Dg" target="_blank">YouTube</a></u>
-   |  爵 <u><a href="https://archiveofourown.org/users/SteamWolf" target="_blank">Ao3</a></u>  
+   |  ♦ <u><a href="mailto:${config.email}" target="_blank">${config.email}</a></u> 
+    | ♦ <u><a href="https://github.com/${config.social.github}" target="_blank">Github</a></u>
+   |  ♦ <u><a href="https://linkedin.com/in/${config.social.linkedin}" target="_blank">Linkedin</a></u>
+    | ♦ <u><a href="https://www.reddit.com/user/SteamWo1f" target="_blank">Reddit</a></u>
+   |  ♦ <u><a href="https://twitter.com/SteamWolf5" target="_blank">Twitter</a></u>
+    | ♦ <u><a href="https://www.twitch.tv/steamwo1f" target="_blank">Twitch</a></u>
+   |  ♦ <u><a href="https://steamwolf.bandcamp.com/" target="_blank">Music</a></u>
+    | ♦ <u><a href="https://www.tumblr.com/blog/steamwo1f" target="_blank">Tumblr</a></u>
+   |  ♦ <u><a href="https://discord.com/invite/XBz9hJAxzD" target="_blank">Discord Server</a></u>
+    | ♦ <u><a href="https://www.youtube.com/channel/UCJqYptKIPNtsWRYWJZZH5Dg" target="_blank">YouTube</a></u>
+   |  ♦ <u><a href="https://archiveofourown.org/users/SteamWolf" target="_blank">Ao3</a></u>  
     | -----------
    |   DONATE 
     |  <u><a href="https://paypal.me/SteamWo1f" target="_blank">PayPal</a></u>
